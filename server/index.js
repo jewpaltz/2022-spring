8a5a8928a48689ecb47f643ebd3668883125f179
Moshe Plotkin <mplotkin@jewpaltz.com>
2022-02-17 15:19:09 -0500

    added nodemon

--- server/index.js
+++ server/index.js
@@ -1,14 +1,24 @@
-const http = require('http');
+const express = require('express')
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const app = express()
+const port = 3000
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+app
+    .get('/', (req, res) => {
+        res.send('You are on the homepage');
+    })
+    .get('/about', (req, res) => {
+        res.send('You are on the about page');
+    })
+    .get('/contact', (req, res) => {
+        res.send({
+            email: 'plotkinm@newpaltz.edu',
+            phone: '123-456-7890',
+            twitter: '@jewpaltz',
+            instagram: '@jewpaltz'
+        });
+    })
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
