0ea6bc5fb2fbbda1ae9c031c9d2af89816a69dd6
Moshe Plotkin <mplotkin@jewpaltz.com>
2022-03-07 15:45:53 -0500

    using pinia for messages

--- client/src/pages/Wall.vue
+++ client/src/pages/Wall.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { ref, reactive, onMounted } from 'vue';
+import Messages from '../components/Messages.vue';
 
 
     const message = ref( 'Hello Vue!' );
@@ -107,10 +108,7 @@ import { ref, reactive, onMounted } from 'vue';
                             </div>
                           </div>
 
-                        <div v-for=" (x, i) in notifications" :class="`notification is-${x.type}`">
-                            <button class="delete" @click="close(i)" ></button>
-                            {{ x.message }}
-                        </div>
+                        <Messages />
                         
                     </div>
                        
