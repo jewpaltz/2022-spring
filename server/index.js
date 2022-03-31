77fe31a060c86e2be972b949559bae899f63728e
jewpaltz <plotkinm@newpaltz.edu>
2022-03-31 18:29:00 +0000

    Deploying to node-deploy from @ jewpaltz/2022-spring@442b09d35aa557dfd3656a97833c7bc13e7384cf 🚀

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 
 const usersController = require('./controllers/users');
@@ -14,7 +15,7 @@ app
     .use(express.json())
 
     .get('/api/', (req, res) => {
-        res.send('You are on the homepage');
+        res.send('You are at the root of the API. For the best class ever - ' + process.env.BEST_CLASS_EVER);
     })
     .use('/api/users', usersController)
 
