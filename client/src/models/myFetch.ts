05f6fd49ea01a1a68861ddfbfd4d59a42a842fea
Moshe Plotkin <mplotkin@jewpaltz.com>
2022-05-05 16:24:39 -0400

    VITE_API_URL

--- client/src/models/myFetch.ts
+++ client/src/models/myFetch.ts
@@ -1,6 +1,6 @@
 /* B"H
 */
-
+                // Use the enviroment variable to set the base url so that the client can be run on different machines
 const API_URL = import.meta.env.VITE_API_URL ?? 'http://localhost:3001/api/';
 
 export function api(url: string, body?: any, method?: string, headers?: HeadersInit) {
